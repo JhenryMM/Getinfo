cf6931d4aa436a92249aba033a379f8c4ae75ddf
Jhenrymm <jhenrymamani51@gmail.com>
2023-10-27 20:15:15 -0300

    se agrego el endpoint de preguntas, falta guardaarlo en la base de datos

--- src/router/pregunta.py
+++ src/router/pregunta.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 
 pregunta = Blueprint('pregunta', __name__)
 
-@pregunta.route('/vcpregunta')
+@pregunta.route('/vcpregunta', methods = ['POST','GET'])
 @login_required
 def vcpregunta():
-    return render_template('auth/vcpregunta.html')
+    if request.method == 'POST':
+        problema = request.form['problema']
+        print(problema)
+        return render_template('auth/vcpregunta.html')
+    else:
+        return render_template('auth/vcpregunta.html')
