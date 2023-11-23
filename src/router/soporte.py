bb23459535646bb5a3ceeae9c9449d77a681d1a0
Jhenrymm <jhenrymamani51@gmail.com>
2023-11-22 21:31:51 -0300

    el usuario soporte ya se puede eliminar tambien

--- src/router/soporte.py
+++ src/router/soporte.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, current_app, flash, current_app, jsonify
+from flask import Blueprint, render_template, request, current_app, flash, current_app, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 from models.ModelUser import ModelUser
 from utils.utils import roles_required
@@ -29,6 +29,8 @@ def crear_soporte():
             user = User(0, username, password_hash, fullname," " ,esadmi)
             ModelUser.crear_soporte(user, db)
             flash('Cuenta creada!')
+
+        return redirect(url_for('soporte.crear_soporte'))
         
     return render_template('auth/crearsoporte.html')
 
@@ -58,7 +60,6 @@ def modificar_soporte(usuario_id):
         db = current_app.db 
         #la nueva contraseña del formulario enviado desde el frontend
         nueva_contrasena = request.json.get('nuevaContrasena')
-        print("la contra en backend: " + nueva_contrasena)
         contraseña_hash = User.generar_hash(nueva_contrasena)
         try:
             cursor = db.connection.cursor()
@@ -68,5 +69,33 @@ def modificar_soporte(usuario_id):
             cursor.close()
             print("llego a guardarlo creo")
             return jsonify({'message': 'Contraseña actualizada correctamente'})
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+
+@soporte.route('/eliminar_soporte/<int:usuario_id>', methods=['DELETE'])
+@login_required
+@roles_required(['soporte'])
+def eliminar_soporte(usuario_id):
+    if request.method == 'DELETE':
+        db = current_app.db 
+        print("El usuario a eliminar es:", usuario_id)
+        try:
+            cursor = db.connection.cursor()
+            sql = "DELETE FROM soportet WHERE idsoportet = %(id)s"
+            cursor.execute(sql, {'id': usuario_id})
+            db.connection.commit()
+            cursor.close()
+            print("Usuario eliminado")
+
+            # Obtener la lista actualizada de usuarios de soporte
+            sql_get_users = """SELECT idsoportet, fullname, username, esadmi FROM soportet WHERE idsoportet != %s"""
+            cursor = db.connection.cursor()
+            cursor.execute(sql_get_users, (current_user.id,))
+            usuarios_soporte = cursor.fetchall()
+            cursor.close()
+
+            # Enviar la lista actualizada como parte de la respuesta JSON
+            return jsonify({'message': 'Se ha eliminado al usuario', 'usuarios': usuarios_soporte})
         except Exception as e:
             return jsonify({'error': str(e)}), 500
